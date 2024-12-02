6953cb8287962388e00bf8ad7d2ae4c6809adef4
Byndum <Mikael.List-Jensen@hotmail.dk>
2024-12-02 13:47:51 +0100

    adding testcase for CalcMethod()

--- SumCalculatorWpfNUnit/UnitTest1.cs
+++ SumCalculatorWpfNUnit/UnitTest1.cs
@@ -1,24 +1,45 @@
 using SumCalculatorWpf.Entitites;
 using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+using System.Collections;
 
 namespace SumCalculatorWpfNUnit
 {
     public class Tests
     {
-        public MethodSelectionInfo selectionInfo;
+        public static IEnumerable TestCases 
+        {
+            get
+            {
+                yield return new TestCaseData(new MethodSelectionInfo(50.0, 70.0, "comfort", 10, 25.0)).SetName("tihi");
+                yield return new TestCaseData(new MethodSelectionInfo(50.0, 70.0, "danger", 10, 25.0)).SetName("fuck off");
+            }
+        }
+        public static IEnumerable TestCasesCalcMethod 
+        {
+            get
+            {
+                yield return new TestCaseData(new MethodSelectionInfo(50.0, 70.0, "comfort", 10, 25.0)).SetName("tihi");
+                yield return new TestCaseData(new MethodSelectionInfo(50.0, 70.0, "danger", 10, 25.0)).SetName("fuck off");
+            }
+        }
         
         [SetUp]
         public void Setup()
         {
-            selectionInfo = new MethodSelectionInfo(50.0,70.0,"comfort",10,25.0);
         }
 
         
-        [Test]
-        public void MethodologiesTest() 
+        [Test, TestCaseSource(nameof(TestCases))]
+        public void MethodologiesTest(MethodSelectionInfo methodSelectionInfo) 
         {
-            MethodSelectionInfo methodSelectInfo = new MethodSelectionInfo(50.0,70.0,"comfort",10,25.0);
-            Assert.That(methodSelectInfo, Is.EqualTo(selectionInfo)); 
+            Assert.That(methodSelectionInfo.Criticality, Is.EqualTo("comfort"));
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesCalcMethod))]
+        public string MethodologiesTest()
+        {
+            Assert.That(methodSelectionInfo.Criticality, Is.EqualTo("comfort"));
         }
     }
 }
