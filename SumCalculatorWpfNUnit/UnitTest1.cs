6d5361efba673ffd91ad1e3251225430e4e76c5f
JannickWAndresen <148877603+JannickWAndresen@users.noreply.github.com>
2024-12-02 10:49:10 +0100

    Add project files.

---
+++ SumCalculatorWpfNUnit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SumCalculatorWpfNUnit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
